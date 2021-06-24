fefa53d856824d36ea1d12927b9990e55fc59888
saurabhkawade784 <saurabhkawade784@gmail.com>
2021-06-24 16:08:46 +0530

    Doublet combinations...

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,14 +2,13 @@
 
 echo "Welcome to flipCoinCombination problem."
 
-echo "Welcome to flip coin combination problem."
-#Variables
 Head=1;
 Tail=0;
 Count_Of_Heads=0;
 Count_Of_Tails=0;
 #diactionary declaration
 declare -A Singlet
+declare -A Doublet
 for (( i=0; i<=5; i++ ))
 do
 
@@ -28,4 +27,28 @@ done
 for pos in ${!Singlet[@]}
 do
 	echo "$pos is: ${Singlet[$pos]},"
+done
+
+#Doublet=(["HH"]=0 ["HT"]=0 ["TH"]=0 ["TT"]=0)
+for ((j=1;j<=5;j++))
+do
+        flip=$((RANDOM%4))
+        case $flip in
+                0) x=${Doublet["HH"]}
+		   Doublet["HH"]=$((++x))
+                	;;
+                1)  x=${Doublet["HT"]}
+                   Doublet["HT"]=$((++x))
+                        ;;
+		2) x=${Doublet["TH"]}
+                   Doublet["TH"]=$((++x))
+                        ;;
+                3) x=${Doublet["TT"]}
+                   Doublet["TT"]=$((++x))
+                        ;;
+        esac
+done
+for index in ${!Doublet[@]}
+do
+        echo "$index: ${Doublet[$index]},"
 done
