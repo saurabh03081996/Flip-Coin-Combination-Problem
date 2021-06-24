77afa79f525b43bae3c740257be42c7985dc4e6b
saurabhkawade784 <saurabhkawade784@gmail.com>
2021-06-24 16:03:56 +0530

    singlet shows heads or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,14 +2,30 @@
 
 echo "Welcome to flipCoinCombination problem."
 
-Head=1
-Tail=0
+echo "Welcome to flip coin combination problem."
+#Variables
+Head=1;
+Tail=0;
+Count_Of_Heads=0;
+Count_Of_Tails=0;
+#diactionary declaration
+declare -A Singlet
+for (( i=0; i<=5; i++ ))
+do
 
-flipCoin=$((RANDOM%2))
-
-if [[ $Head -eq $flipCoin ]]
-then
-	echo "It's head..."
-else
-	echo "It's tail..."
-fi
+	Flip_Coin=$((RANDOM%2))
+	case $Flip_Coin in
+		$Head) Singlet["Heads"]=$((++Count_Of_Heads)) 
+		    ;;
+		$Tail) Singlet["Tails"]=$((++Count_Of_Tails)) 
+		    ;;
+		    *) echo  " "
+		    ;;
+	esac
+	((No_Of_Times--))
+done
+#read dictinaery elements.
+for pos in ${!Singlet[@]}
+do
+	echo "$pos is: ${Singlet[$pos]},"
+done
