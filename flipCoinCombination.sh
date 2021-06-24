e65ba1b435ea40041cc457943e4517ce2104f9c8
saurabhkawade784 <saurabhkawade784@gmail.com>
2021-06-24 15:58:11 +0530

    Flipping coin for toss.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,15 @@
 #! /bin/bash -x
 
 echo "Welcome to flipCoinCombination problem."
+
+Head=1
+Tail=0
+
+flipCoin=$((RANDOM%2))
+
+if [[ $Head -eq $flipCoin ]]
+then
+	echo "It's head..."
+else
+	echo "It's tail..."
+fi
